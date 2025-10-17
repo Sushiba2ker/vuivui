a2cb89f0a2fcb8c7f1d4f04aad916ae824a95ac3
Sushiba2ker <nqb13052003@gmail.com>
2025-10-17 08:57:49 +0700

    first commit

---
+++ vuivui.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         VuiVui Full VIP
+// @namespace    http://tampermonkey.net/
+// @version      1.7
+// @description  Bypass VIP + Coin
+// @match        *://www.rophim.me/*
+// @match        *://www.rophim.mx/*
+// @match        *://www.rophim.li/*
+// @run-at       document-start
+// @grant        none
+// @require      https://raw.githubusercontent.com/your-username/your-repo/main/Bypas-vip-rophim.js
+// ==/UserScript==
